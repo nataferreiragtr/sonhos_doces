de8062cf3ceb381bc5708f89b1a420cf74a2df34
marhvi <mvjs2004@gmail.com>
2022-12-01 21:45:02 -0300

    first commit

---
+++ src/main/java/com/rj/doceria/docesSonhos/DocesSonhosApplication.java
@@ -0,0 +1,13 @@
+package com.rj.doceria.docesSonhos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocesSonhosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocesSonhosApplication.class, args);
+	}
+
+}
